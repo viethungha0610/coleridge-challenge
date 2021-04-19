424466e3f1be9aca73cafd2fa50a56400dc4a3ab
Ha Viet Hung <viethungha0610@gmail.com>
2021-04-19 23:51:07 +0100

    Initial modelling codes added

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import transformers
+
+MAX_LEN = 128
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 8
+EPOCHS = 1
+BASE_MODEL_PATH = 'bert-base-uncased'
+MODEL_PATH = 'model.bin'
+TRAINING_FILE = '../input/ner_data.csv'
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH,
+    do_lower_case=True
+)
