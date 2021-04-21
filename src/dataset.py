066a022cadb44c3b8af487d6bc8eaa70bf227b3f
Ha Viet Hung <viethungha0610@gmail.com>
2021-04-21 08:20:03 +0100

    Added predict.py and changeed dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -25,25 +25,25 @@ class EntityDataset:
             ids.extend(inputs)
             target_tag.extend([tags[i]] * input_len)
 
-            ids = ids[:config.MAX_LEN - 2]
-            target_tag = target_tag[:config.MAX_LEN - 2]
+        ids = ids[:config.MAX_LEN - 2]
+        target_tag = target_tag[:config.MAX_LEN - 2]
 
-            ids = [101] + ids + [102]
-            target_tag = [0] + target_tag + [0]
+        ids = [101] + ids + [102]
+        target_tag = [0] + target_tag + [0]
 
-            mask = [1] * len(ids)
-            token_type_ids = [0] * len(ids)
+        mask = [1] * len(ids)
+        token_type_ids = [0] * len(ids)
 
-            padding_len = config.MAX_LEN - len(ids)
+        padding_len = config.MAX_LEN - len(ids)
 
-            ids = ids + ([0] * padding_len)
-            mask = mask + ([0] * padding_len)
-            token_type_ids = token_type_ids + ([0] * padding_len)
-            target_tag = target_tag + ([0] * padding_len)
+        ids = ids + ([0] * padding_len)
+        mask = mask + ([0] * padding_len)
+        token_type_ids = token_type_ids + ([0] * padding_len)
+        target_tag = target_tag + ([0] * padding_len)
 
-            return {
-                "ids": torch.tensor(ids, dtype=torch.long),
-                "mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
-                "target_tag": torch.tensor(target_tag, dtype=torch.long),
-            }
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "target_tag": torch.tensor(target_tag, dtype=torch.long),
+        }
